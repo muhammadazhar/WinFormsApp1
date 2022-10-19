dd2891124a648e55fb432757b3526fad9805a94e
Muhammad Azhar <mh.azhar75@gmail.com>
2022-10-19 15:41:10 +0300

    Adding second close button

--- WinFormsApp1/Form1.Designer.cs
+++ WinFormsApp1/Form1.Designer.cs
@@ -29,6 +29,7 @@
         private void InitializeComponent()
         {
             this.btnClose = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // btnClose
@@ -41,11 +42,21 @@
             this.btnClose.UseVisualStyleBackColor = true;
             this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(184, 156);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(95, 45);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Close";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(479, 227);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.btnClose);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -56,5 +67,6 @@
         #endregion
 
         private Button btnClose;
+        private Button button1;
     }
 }
