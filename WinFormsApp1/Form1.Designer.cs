5e02bdedb181c42087c860fe1d24e755320114c6
Muhammad Azhar <mh.azhar75@gmail.com>
2022-10-19 16:45:23 +0300

    Remove all three buttons

--- WinFormsApp1/Form1.Designer.cs
+++ WinFormsApp1/Form1.Designer.cs
@@ -29,9 +29,6 @@
         private void InitializeComponent()
         {
             this.btnClose = new System.Windows.Forms.Button();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // btnClose
@@ -44,41 +41,11 @@
             this.btnClose.UseVisualStyleBackColor = true;
             this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
             // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(184, 156);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(95, 45);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "Close";
-            this.button1.UseVisualStyleBackColor = true;
-            // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(83, 156);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(95, 45);
-            this.button2.TabIndex = 2;
-            this.button2.Text = "Close";
-            this.button2.UseVisualStyleBackColor = true;
-            // 
-            // button3
-            // 
-            this.button3.Location = new System.Drawing.Point(296, 156);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(95, 45);
-            this.button3.TabIndex = 3;
-            this.button3.Text = "Close";
-            this.button3.UseVisualStyleBackColor = true;
-            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(479, 227);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
             this.Controls.Add(this.btnClose);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -89,8 +56,5 @@
         #endregion
 
         private Button btnClose;
-        private Button button1;
-        private Button button2;
-        private Button button3;
     }
 }
